0e5c95f89d572c2241a3f1012f9aec61747b1abc
indaun <indaadelin@gmail.com>
2026-04-26 15:08:30 +0900

    add: github actions build APK

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -4,14 +4,13 @@ plugins {
     id("com.google.gms.google-services")
     // END: FlutterFire Configuration
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
     namespace = "com.example.sikp_mobile"
-    compileSdk = 34
-    ndkVersion = "27.0.12077973"
+    compileSdk = 35
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -23,24 +22,15 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.sikp_mobile"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-<<<<<<< HEAD
         minSdk = flutter.minSdkVersion
-=======
-        minSdk = 21
->>>>>>> c43980431e21a2115dc2a2e0f27101105c2bd342
-        targetSdk = 34
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -49,16 +39,12 @@ android {
 flutter {
     source = "../.."
 }
+
 subprojects {
     configurations.all {
         resolutionStrategy {
-            // Memaksa versi core yang sudah punya atribut lStar
             force("androidx.core:core:1.13.1")
             force("androidx.core:core-ktx:1.13.1")
         }
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> c43980431e21a2115dc2a2e0f27101105c2bd342
