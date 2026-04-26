bcfd77742e0daa0902a650cec879d71b6c9fb587
indaun <indaadelin@gmail.com>
2026-04-26 15:50:52 +0900

    fix: update minSdk ke 23

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.sikp_mobile"
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
